ad1c8373d605c46fcbcee901d898729df7e0a765
felipeafbruno <felipedobru@gmail.com>
2019-02-15 15:22:50 -0200

    Recuperar senha funcionando corretamente

--- ecommerce/vendor/composer/autoload_static.php
+++ ecommerce/vendor/composer/autoload_static.php
@@ -6,8 +6,26 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitfb847b6ea07d13c59a2f12d34f3c7c79
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -27,23 +45,12 @@ class ComposerStaticInitfb847b6ea07d13c59a2f12d34f3c7c79
         ),
     );
 
-    public static $classMap = array (
-        'EasyPeasyICS' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
-        'PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
-        'PHPMailerOAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauth.php',
-        'PHPMailerOAuthGoogle' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
-        'POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.pop3.php',
-        'SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.smtp.php',
-        'ntlm_sasl_client_class' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
-        'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
-    );
-
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInitfb847b6ea07d13c59a2f12d34f3c7c79::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitfb847b6ea07d13c59a2f12d34f3c7c79::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitfb847b6ea07d13c59a2f12d34f3c7c79::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitfb847b6ea07d13c59a2f12d34f3c7c79::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInitfb847b6ea07d13c59a2f12d34f3c7c79::$classMap;
 
         }, null, ClassLoader::class);
     }
